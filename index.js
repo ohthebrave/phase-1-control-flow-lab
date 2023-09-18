be49ba5be33c1bb5ecf19b9acff5ca606d8bdc2b
George <georgejoj28@gmail.com>
2023-09-18 16:06:01 +0300

    Completed

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distanceFeet) {
+  if (distanceFeet <= 400) {
+    return "This one is on me!";
+  } else if (distanceFeet > 2000 && distanceFeet <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === "NYC" ? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case 'generous':
+      return "Thank you so much.";
+    case 'not as generous':
+      return "Thank you.";
+    default:
+      return "Bye."  
+  }
 }
